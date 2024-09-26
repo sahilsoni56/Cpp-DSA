ac81c3c065715658ba34b168146784dde9c0084b
sahilsoni56 <119436049+sahilsoni56@users.noreply.github.com>
2024-09-26 19:22:33 +0530

    2 stack in one aray

---
+++ 2 stack in 1 array/st.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+
+using namespace std;
+class Stack{
+    public:
+int* arr;
+int size;
+int top1;
+int top2;
+
+    Stack(int size){
+        arr = new int[size];
+        this->size = size;
+        top1=-1;
+        top2 =size;
+    }
+    void push1(int data){
+        if(top2-top1 == 1){
+            cout<<"OVERFLOWN"<<endl;
+        }else{
+            top1++;
+            arr[top1] = data;
+        }
+    }
+    void push2(int data){
+        if(top2-top1 == 1){
+            cout<<"OVERFLOWN"<<endl;
+        }else{
+             top2--;
+        arr[top2] = data;
+        }
+    }
+    void pop1(int data){
+        if(top1 == -1){
+            cout<<"UNDERFLOWN"<<endl;
+        }else{
+        arr[top1] = 0;
+        top1--;
+        }
+    }
+    void pop2(int data){
+        if(top2 == size){
+            cout<<"UNDERFLOWN"<<endl;
+        }else{
+        arr[top2] = 0;
+        top2++;
+        }
+    }
+    
+    void print(){
+        cout<<top1<<endl;
+        cout<<top2<<endl;
+        for(int  i =0 ; i<size;i++){
+            cout<<arr[i]<<" ";
+        }
+    }
+};
+
+int main() {
+    Stack st(6);
+    
+    st.push1(10);
+    st.push1(20);
+    st.push1(30);
+    st.print();
+    
+    st.push2(100);
+    st.print();
+    
+     st.push2(200);
+     st.push2(300);
+     st.push2(200);
+    st.print();
+    return 0;
+}
